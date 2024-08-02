435a770294ca1d61c2ce09e6267f7367bec1c0bd
VaradAvhad <avhadvarad2001@gmail.com>
2024-08-02 13:39:48 +0530

    New Entites added.

--- eTour/src/main/java/com/example/model/Customer.java
+++ eTour/src/main/java/com/example/model/Customer.java
@@ -34,6 +34,7 @@ public class Customer
 	public String getCustomer_name() {
 		return customer_name;
 	}
+	
 	public void setCustomer_name(String customer_name) {
 		this.customer_name = customer_name;
 	}
