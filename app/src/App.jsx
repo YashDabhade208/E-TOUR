93410ee320023f8e3e2185da07f0f14a4741e88f
Jayesh-2001 <jayeshborole2002@gmail.com>
2024-08-02 15:07:22 +0530

    Login and Register page

---
+++ app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Main from './components/Main';
+import './App.css'
+
+
+function App() {
+  return (
+    <div>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
+
