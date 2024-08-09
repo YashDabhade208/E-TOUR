6591f5e6cd56a32faef586112f4a34c5a9c7e141
VaradAvhad <avhadvarad2001@gmail.com>
2024-08-09 13:13:07 +0530

    Bug Fixed

--- eTour/src/main/java/com/example/service/BookingServiceImpl.java
+++ eTour/src/main/java/com/example/service/BookingServiceImpl.java
@@ -4,65 +4,60 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.model.Booking;
+import com.example.model.Customer;
 import com.example.model.Tour;
 import com.example.repository.BookingRepository;
+import com.example.repository.CustomerRepository;
 import com.example.repository.TourRepository;
 
-
 @Service
-public class BookingServiceImpl implements BookingService
-{
+public class BookingServiceImpl implements BookingService {
 
     @Autowired
     private BookingRepository bookingRepository;
 
-    public List<Booking> getAllBookings() {
-        return bookingRepository.findAll();
-    }
-
-    public Booking getBookingById(Integer id) {
-        return bookingRepository.findById(id).orElseThrow();
-    }
+    @Autowired
+    private CustomerRepository customerRepository;
 
     @Autowired
     private TourRepository tourRepository;
 
+    @Override
+    @Transactional
     public Booking createBooking(Booking booking) {
+        // Ensure Customer is persisted
+        Customer customer = customerRepository.save(booking.getCustomer_id());
+        booking.setCustomer_id(customer);
+
+        // Ensure Tour is persisted
         Tour tour = tourRepository.save(booking.getSubcategory_id());
         booking.setSubcategory_id(tour);
+
         return bookingRepository.save(booking);
     }
 
+	@Override
+	public Booking getBookingById(Integer id) {
+		 return bookingRepository.findById(id).orElse(null);
+	}
 
-    public Booking updateBooking(Integer id, Booking booking) {
-        Booking existingBooking = getBookingById(id);
-        existingBooking.setFirstname(booking.getFirstname());
-        existingBooking.setLastname(booking.getLastname());
-        existingBooking.setAge(booking.getAge());
-        existingBooking.setMobileno(booking.getMobileno());
-        existingBooking.setNumberofpassenger(booking.getNumberofpassenger());
-        existingBooking.setGender(booking.getGender());
-        existingBooking.setDateofbirth(booking.getDateofbirth());
-        existingBooking.setEmail(booking.getEmail());
-        existingBooking.setAdhaarno(booking.getAdhaarno());
-        existingBooking.setAdress(booking.getAdress());
-        existingBooking.setCity(booking.getCity());
-        existingBooking.setState(booking.getState());
-        existingBooking.setCountry(booking.getCountry());
-        existingBooking.setPincode(booking.getPincode());
-        existingBooking.setNumberofchild(booking.getNumberofchild());
-        existingBooking.setNumberofchildwithbed(booking.getNumberofchildwithbed());
-        existingBooking.setNumberofchildwithoutbed(booking.getNumberofchildwithoutbed());
-        existingBooking.setBookingdate(booking.getBookingdate());
-        existingBooking.setCustomer_id(booking.getCustomer_id());
-        existingBooking.setSubcategory_id(booking.getSubcategory_id());
-        return bookingRepository.save(existingBooking);
-    }
-    
+	@Override
+	public List<Booking> getAllBookings() {
+		 return bookingRepository.findAll();
+	}
 
-    public void deleteBooking(Integer id) {
-        bookingRepository.deleteById(id);
-    }
+	@Override
+	public Booking updateBooking(Integer id, Booking booking) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteBooking(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
 }
